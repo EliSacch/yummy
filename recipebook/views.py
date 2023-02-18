2af428628008a5eb0ef847df3c1fa908d1807108
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-18 11:32:28 +0000

    Create basic templates

--- recipebook/views.py
+++ recipebook/views.py
@@ -5,5 +5,6 @@ from .models import Recipe
 class RecipeListView(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.order_by('created_on')
+    template_name = 'index.html'
     paginate_by = 12
 
