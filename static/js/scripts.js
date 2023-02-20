caf390f7ae3e074d735a330742f2f324ad5f4bc1
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-20 15:48:42 +0000

    Implement go-back button

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     /* Materialize CSS Inizialization */
     $('.modal').modal();
 
+
+    /* Custom JS */
+    $('.go-back').click(function(){
+        window.history.back();
+    });
+
   });
