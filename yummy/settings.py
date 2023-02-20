75b5344666431394543b8b4f631ec9f0148c2dc5
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-20 17:00:06 +0000

    Add Same origin configuration

--- yummy/settings.py
+++ yummy/settings.py
@@ -32,6 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 ALLOWED_HOSTS = ['Yummy-recipe-book.herokuapp.com', '127.0.0.1']
 
 
