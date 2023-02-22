d0bd99591a13e3cc63aab34053ab189eee5699bc
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-22 22:16:23 +0000

    Create suggestions section

--- recipebook/views.py
+++ recipebook/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Recipe
 
+
 class RecipeListView(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.order_by('created_on')
     template_name = 'index.html'
     paginate_by = 12
+    
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeListView, self).get_context_data(**kwargs)
+        all_recipes = Recipe.objects.all()
+        context['suggestions'] = all_recipes.order_by('?')[:2]
+        return context
+
