70c3db786ca9416120a031af51d461ffc534f0d8
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-24 23:21:13 +0000

    Edit ingredients field to add multiple ingredients

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -11,4 +11,31 @@ $(document).ready(function(){
         window.history.back();
     });
 
+
+    $('#add-ingredient-btn').click(function(){
+        var ingredients = []
+        var ingredient_name = $('#ingredient-name').val();
+        var amount = $('#amount').val();
+        var unit = $('#unit').val();
+
+        var ingredient_line = '<div class="ingredient-line row">'
+        +'<div class="col s1"><button class="remove-ingredient btn-floating">'
+        +'<i class="small material-icons">remove</i>'
+        +'</button></div>'
+        +'<div class="col s11">' 
+        + ingredient_name + ': ' 
+        + amount + ' ' 
+        + unit 
+        + '</div></div>';
+
+        $('#ingredients-list').append(ingredient_line);
+        $('#ingredient-name').val('');
+        $('#amount').val('');
+        $('#unit').val('');
+        ingredients.push({'name': ingredient_name, 'amount' : amount, 'unit': unit});
+        for(ingredient of ingredients) {
+            console.log(ingredient.name, ingredient.amount, ingredient.unit)
+        }
+    });
+
   });
