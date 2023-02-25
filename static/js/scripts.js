7c9008b8c96330c056f375c2bc166130d9367c7d
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-02-25 13:01:25 +0000

    Create function to remove ingrient item from list

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,30 +12,49 @@ $(document).ready(function(){
     });
 
 
+    var ingredients = [];
+
     $('#add-ingredient-btn').click(function(){
-        var ingredients = []
-        var ingredient_name = $('#ingredient-name').val();
-        var amount = $('#amount').val();
-        var unit = $('#unit').val();
 
-        var ingredient_line = '<div class="ingredient-line row">'
-        +'<div class="col s1"><button class="remove-ingredient btn-floating">'
-        +'<i class="small material-icons">remove</i>'
-        +'</button></div>'
-        +'<div class="col s11">' 
-        + ingredient_name + ': ' 
-        + amount + ' ' 
-        + unit 
-        + '</div></div>';
+        const ingredient_name = $('#ingredient-name').val();
+        const amount = $('#amount').val();
+        const unit = $('#unit').val();
 
-        $('#ingredients-list').append(ingredient_line);
+        ingredients.push({'name': ingredient_name, 'amount' : amount, 'unit': unit});
+        show_added_ingredients(ingredients);
+
         $('#ingredient-name').val('');
         $('#amount').val('');
         $('#unit').val('');
-        ingredients.push({'name': ingredient_name, 'amount' : amount, 'unit': unit});
-        for(ingredient of ingredients) {
-            console.log(ingredient.name, ingredient.amount, ingredient.unit)
-        }
+
     });
 
   });
+
+  function show_added_ingredients(ingredients){
+    $('#ingredients-list').empty();
+    for(var i = 0; i < ingredients.length; i++) {
+        var ingredient_line = `
+        <div class="ingredient-line row">
+            <div class="col s1">
+                <button type="button" class="remove-ingredient btn-floating" data-index="${i}">
+                    <i class="small material-icons">remove</i>
+                </button>
+            </div>
+            <div class="col s11">
+                ${ingredients[i].name}: ${ingredients[i].amount} ${ingredients[i].unit} 
+            </div>
+        </div>`;
+        $('#ingredients-list').append(ingredient_line);
+    }
+
+    $('.remove-ingredient').click(function(){
+        index = $(this).data('index');
+        remove_ingredient(ingredients, index);
+    });
+ }
+ 
+ function remove_ingredient(ingredients, index){
+    ingredients.splice(index, 1);
+    show_added_ingredients(ingredients);
+}
