2ecd3536d0376f0677395c430b0be3c92d775674
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-03-30 09:23:02 +0100

    Set debug to false

--- yummy/settings.py
+++ yummy/settings.py
@@ -35,7 +35,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
